8eba948b5a589a42fbb22ebc214ce3a0762654e0
gizemgerekli <163686175+gizemgerekli@users.noreply.github.com>
2024-06-09 16:42:49 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
-import 'HomePage/HomePage.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:movies_app/core/domain/entities/media.dart';
+import 'package:movies_app/core/resources/app_router.dart';
+import 'package:movies_app/core/services/service_locator.dart';
+import 'package:movies_app/core/resources/app_strings.dart';
+import 'package:movies_app/core/resources/app_theme.dart';
+import 'package:movies_app/watchlist/presentation/controllers/watchlist_bloc/watchlist_bloc.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(MediaAdapter());
+  await Hive.openBox('items');
+  ServiceLocator.init();
+
+  runApp(
+    BlocProvider(
+      create: (context) => sl<WatchlistBloc>(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        useMaterial3: true,
-      ),
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: const HomePage(),
+      title: AppStrings.appTitle,
+      theme: getApplicationTheme(),
+      routerConfig: AppRouter().router,
     );
   }
 }
